7935ff608e2035a48d33541512ca7f26969a1043
NathanRodet <64322615+NathanRodet@users.noreply.github.com>
2021-03-09 21:32:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const Bot = new Discord.Client()
+
+// token ODE4OTM1OTE2MDAwMTgyMzAy.YEfTgQ.d4OkLgvHBwue9TE8iyvPXmR_H9g
+
+// Connexion BOT
+
+Bot.login('ODE4OTM1OTE2MDAwMTgyMzAy.YEfTgQ.d4OkLgvHBwue9TE8iyvPXmR_H9g')
