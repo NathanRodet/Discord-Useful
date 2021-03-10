56cb7deee328930ebabdf835749c1a5a2b604751
Nureg <32011164+CopinFrancois@users.noreply.github.com>
2021-03-10 22:10:15 +0100

    ajout message arrivée

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Discord = require("discord.js");
 
 const Bot = new Discord.Client();
 Bot.login("ODE4OTM1OTE2MDAwMTgyMzAy.YEfTgQ.d4OkLgvHBwue9TE8iyvPXmR_H9g");
+//NzIyOTE0NDExNTUxMTI5NjQx.XuqAgg.CFJfIueEXz-nLpVzGbMs2hhHq1I
+//ODE4OTM1OTE2MDAwMTgyMzAy.YEfTgQ.d4OkLgvHBwue9TE8iyvPXmR_H9g
 
 Bot.on("ready", () => {
   console.log(`Bot on`);
@@ -30,6 +32,28 @@ Bot.on("message", function (message) {
     //message.reply('pong')
     message.channel.send("Pong !");
   }
+});
+
+// Arrivée membre
+
+Bot.on("guildMemberAdd", (member) => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "général"
+  );
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Bienvenue ${member} !
+
+Si tu as la moindre question, n'hésite pas à la poser à un valet ou à un invité d'honneur/privilégié !
+  
+Je t'invite aussi à aller jeter un œil au <#744641300057424002> pour te tenir informé des règles de ce Discord !
+  
+Aussi, si tu souhaites obtenir le grade Invité, je te recommande de faire ta présentation dans #présentation en suivant le modèle du règlement !
+  
+Bon séjour parmi nous !`);
+  member.roles.add("819294604255297566");
 });
 
 // Disconnect the bot
